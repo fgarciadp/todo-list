e2727115242d924a3d529794a0bee6d6f3089ab2
Felipe Garcia <22felipe95@gmail.com>
2023-08-01 15:38:24 -0400

    Delete action added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Todo from './Todo.js'
 
 export const ACTIONS = {
   ADD_TODO: 'add-todo',
-  TOGGLE_TODO: 'toggle-todo'
+  TOGGLE_TODO: 'toggle-todo',
+  DELETE_TODO: 'delete-todo'
 };
 
 function reducer(todos, action) {
@@ -20,7 +21,10 @@ function reducer(todos, action) {
         }
         return todo
       })
-    
+    case ACTIONS.DELETE_TODO:
+      return todos.filter(todo => todo.id !== action.payload.id)
+      default: 
+      return todos
   }
 };
 
--- src/Todo.js
+++ src/Todo.js
@@ -7,13 +7,18 @@ export default function Todo({ todo, dispatch }) {
         <span style={{ color: todo.complete ? '#AAA' : '#000' }}>
             {todo.name}
         </span>
+        
         <button onClick={() => dispatch({ 
                 type: ACTIONS.TOGGLE_TODO,
                 payload: { id: todo.id }
             })}>
             ✔ Mark as done
         </button>  
-        <button>
+
+        <button onClick={() => dispatch({ 
+                type: ACTIONS.DELETE_TODO,
+                payload: { id: todo.id }
+            })}>
             ⓧ Delete
         </button>
     </div>
