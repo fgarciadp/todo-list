9d5f0bca48c0e5fb5c9ed9c19e524b913862a19e
Felipe Garcia <22felipe95@gmail.com>
2023-08-01 12:26:30 -0400

    Added state tracking todos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from 'react'
 
-function App() {
+const ACTIONS = {
+  ADD_TODO: 'add-todo'
+};
+
+function reducer(state, action) {
+};
+
+
+export default function App() {
+  const [todos, dispatch] = useReducer(reducer, []);
+  const [name, setName] = useState('');
+
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    dispatch({ type: ACTIONS.ADD_TODO })
+    setName('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type='text' value={name} onChange={e => setName(e.target.value)} />
+      </form>
+    </>
   );
-}
+};
 
-export default App;
+
